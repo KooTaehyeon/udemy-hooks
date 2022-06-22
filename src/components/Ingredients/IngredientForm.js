0f82f2f8fa1fb3155e7ad22c5c826352c72137be
Kth <vcro12123@naver.com>
2022-06-22 19:10:49 +0900

    input state 추가

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,28 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
-const IngredientForm = React.memo(props => {
-  const submitHandler = event => {
+const IngredientForm = React.memo((props) => {
+  const inputState = useState({ title: '', amount: '' });
+  console.log(inputState);
+  const submitHandler = (event) => {
     event.preventDefault();
     // ...
   };
 
   return (
-    <section className="ingredient-form">
+    <section className='ingredient-form'>
       <Card>
         <form onSubmit={submitHandler}>
-          <div className="form-control">
-            <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+          <div className='form-control'>
+            <label htmlFor='title'>Name</label>
+            <input
+              type='text'
+              id='title'
+              value={inputState[0].title}
+              onChange={(e) =>
+                inputState[1]({
+                  title: e.target.value,
+                  amount: inputState[0].amount,
+                })
+              }
+            />
           </div>
-          <div className="form-control">
-            <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+          <div className='form-control'>
+            <label htmlFor='amount'>Amount</label>
+            <input
+              type='number'
+              id='amount'
+              value={inputState[0].amount}
+              onChange={(e) =>
+                inputState[1]({
+                  amount: e.target.value,
+                  title: inputState[0].title,
+                })
+              }
+            />
           </div>
-          <div className="ingredient-form__actions">
-            <button type="submit">Add Ingredient</button>
+          <div className='ingredient-form__actions'>
+            <button type='submit'>Add Ingredient</button>
           </div>
         </form>
       </Card>
